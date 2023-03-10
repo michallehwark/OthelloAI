4caec3778c1dc933d911e4e4f06cee75b9439d0d
michallehwark <michallehwark511@gmail.com>
2023-03-10 14:51:35 +0100

    utility function in AI player

--- AI_player.py
+++ AI_player.py
@@ -1,4 +1,4 @@
-# implement AI
+import numpy as np
 
 def alfaBetaPruning(self, currentBoard, depth=3):
     value, bestMove = maxValueSearch()
@@ -7,7 +7,7 @@ def alfaBetaPruning(self, currentBoard, depth=3):
 def maxValueSearch(self, board, alfa=-float('inf'), beta=float('inf'), depth=3):
 
     if self.TerminalState(board) or depth == 0:
-        return self.GameOver
+        return self.utility()
     
     #get all possible legal moves
     actionList = getAllMoves(board)
@@ -22,3 +22,13 @@ def minValueSearch(self, board, alfa=-float('inf'), beta=float('inf'), depth=3):
         return self.GameOver
     
     return False
+
+def utility(self, currentStateOfBoard, depth):
+    """
+    Function to determine the current state of the game.
+    """
+    boardScore = np.sum(currentStateOfBoard)
+    #lastMoveScore = 
+    finalScore = boardScore #+ lastMoveScore
+
+    return finalScore
