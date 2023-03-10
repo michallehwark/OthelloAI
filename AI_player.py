7ad35e4d442c77b8d5e8a90507fcbdd978562fec
michallehwark <michallehwark511@gmail.com>
2023-03-10 13:23:59 +0100

    sudo code for AI player

--- AI_player.py
+++ AI_player.py
@@ -1,1 +1,24 @@
-# implement AI player
+# implement AI
+
+def alfaBetaPruning(self, currentBoard, depth=3):
+    value, bestMove = maxValueSearch()
+    return True
+
+def maxValueSearch(self, board, alfa=-float('inf'), beta=float('inf'), depth=3):
+
+    if self.TerminalState(board) or depth == 0:
+        return self.GameOver
+    
+    #get all possible legal moves
+    actionList = getAllMoves(board)
+    if actionList == 
+    
+    # minValueSearch
+    return False
+
+def minValueSearch(self, board, alfa=-float('inf'), beta=float('inf'), depth=3):
+
+    if self.TerminalState(board)  or depth == 0:
+        return self.GameOver
+    
+    return False
